3e9522adac516f2938ffa0a709f35173ed1d4563
Arthur Amorim <arthur.amorim@inventione.com.br>
2023-02-28 19:15:30 -0300

    Criaçao de projeto

---
+++ WebAPI/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Photo { get; set; }
+
+        public Employee(string name, int age, string photo)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Photo = photo;
+        }
+    }
+}
